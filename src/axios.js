0ef8020bf63f98aad9c549213d235189e74ae5f5
Atharva Naphade <naphade21@gmail.com>
2021-08-12 00:08:30 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseurl = 'http://127.0.0.1:8000/api';
+export const baseurl = 'https://disc-forum.herokuapp.com/api';
 // export const baseurl = 'https://disc-forum-pict.herokuapp.com/api';
 
 // export const baseurl = 'http://0.0.0.0/api';
